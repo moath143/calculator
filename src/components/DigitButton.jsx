07b8abf461212a9b3bbd512bd23348475caddcb8
Moath Alnajjar <moath2sweet@hotmail.com>
2022-02-08 08:00:25 +0200

    calculator project

---
+++ src/components/DigitButton.jsx
@@ -0,0 +1,3 @@
+export default function DigitButton({ addDigit, digit }) {
+  return <button onClick={() => addDigit(digit)}>{digit}</button>;
+}
---
+++ src/constants/IntegerFormatter.jsx
@@ -0,0 +1,5 @@
+const integerFormatter = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+
+export default integerFormatter;
